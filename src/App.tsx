b38369d6c027b9bf5a81103bfb977313349838ce
FrancoisVdh <francois.vdh@live.be>
2025-02-09 23:09:01 +0100

    WIP

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/Hero.tsx'
+import Navbar from './components/Navbar.tsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+
+    </>
+  )
+}
+
+export default App
