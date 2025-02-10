86e04f0321415c975ca3843e1af3a942d1829bcf
Vanderheeren, François (RC-BE DI CS GRAD) <francois.vanderheeren@siemens.com>
2025-02-10 17:25:31 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Hero from './components/Hero.tsx'
-import Navbar from './components/Navbar.tsx'
+import './App.css';
+import Hero from './components/Hero.tsx';
+import Navbar from './components/Navbar.tsx';
+import About from './components/About.tsx';
+import Services from './components/Services.tsx';
 
 function App() {
-
   return (
     <>
       <div>
         <Navbar />
         <Hero />
+        <About />
+        <Services />
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
